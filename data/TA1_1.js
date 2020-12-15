a230f80e1c5652fd6b9ae8758d3eb02bd52f9365
baiqaftry <74760344+baiqaftry@users.noreply.github.com>
2020-12-16 03:17:03 +0700

    Add files via upload

---
+++ data/TA1_1.js
@@ -0,0 +1,1 @@
+var json_TA1_1 = {"type":"FeatureCollection","name":"TA1_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[107.734092892551956,-6.938385786572237]}}]}
